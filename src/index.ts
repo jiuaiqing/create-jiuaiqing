1b27bc090b71321c4587b13212d27ef354434772
jiuaiqing <1007419921@qq.com>
2024-05-10 20:27:59 +0800

    第一版

---
+++ src/index.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+ 
+import prompts from "prompts";
+import path from "node:path";
+import fs from "node:fs";
+import chalk from "chalk";
+const bootstrap = async () => {
+    console.log(chalk.cyan("欢迎使用久爱卿开发脚手架"));
+    const result =  await prompts([
+        {
+            type: "text",
+            name: "projectName",
+            message: "请输入项目名称:"
+        },
+    ]);
+    //管理控制台输入
+    //————————————————————————————————————————————————————————————————————————————————————————————————
+    const targetPath = path.resolve(process.cwd(), result.projectName);
+    const sourcePath = path.resolve(__dirname, "../template");
+    // Copy files from sourcePath to targetPath
+    fs.cpSync(sourcePath, targetPath, { recursive: true });
+    // Update package.json
+    const packageJsonPath = path.resolve(targetPath, 'package.json');
+    const packageJson = require(packageJsonPath);
+    packageJson.name = result.projectName;
+    // Write the updated package.json back to the file
+    fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
+    // Rename _gitignore to .gitignore
+    fs.renameSync(
+        path.resolve(targetPath, "_gitignore"),
+        path.resolve(targetPath, ".gitignore")
+    );
+    //————————————————————————————————————————————————————————————————————————————————————————————————
+    //最后输出
+    console.log(`
+    项目创建成功，请执行以下命令：
+    cd ${result.projectName}
+    pnpm i
+    pnpm run dev
+    `)
+};
+bootstrap();
